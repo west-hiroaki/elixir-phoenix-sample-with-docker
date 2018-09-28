28672ae19929f8f8b69b099a8aac5b25b983141d
hiroaki.nishi <hiroaki.nish@gu3.co.jp>
2018-09-28 16:00:54 +0900

    phoenixプロジェクト my_app を追加（phx.new my_app）

---
+++ my_app/config/test.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :my_app, MyAppWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :my_app, MyApp.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  username: "root",
+  password: "",
+  database: "my_phoenix_dev",
+  hostname: "127.0.0.1",
+  port: 3336,
+  pool_size: 10
